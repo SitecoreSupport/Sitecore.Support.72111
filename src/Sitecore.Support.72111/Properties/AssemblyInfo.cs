1d1e4e9d0e80fa84112cf02b3475b43ab6077519
Mohammad Chamanpara <mhch@sitecore.net>
2017-02-09 16:06:51 +0800

    Commit Patch for EXM 3.1.1

---
+++ src/Sitecore.Support.72111/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.72111")]
+[assembly: AssemblyProduct("Sitecore.Support.72111")]
+[assembly: ComVisible(false)]
